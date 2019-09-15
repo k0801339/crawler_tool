61e7f8fcd407a7564189cce21ddc15925c1522a1
k0801339 <k0801339@gmail.com>
2019-09-15 16:43:49 +0800

    Support input by command prompt - For 博客來 Parser part

--- crawlPractice.py
+++ crawlPractice.py
@@ -4,6 +4,7 @@ from pprint import pprint
 
 # from multiprocessing import Pool7
 import re
+import os
 
 class SimpleParser():
     '''
@@ -150,18 +151,66 @@ class BooksParser(SimpleParser):
             'author': entry.find("a[rel='go_author']")[0].text,
             'publish': entry.find("a[rel='mid_publish']", first=True).text,
             #'date':  entry.text,
-            'discount': entry.find("span.price strong b", first=True).text,
+            'discount': entry.find("span.price strong b", first=True).text + "折",
             'price': entry.find("span.price strong b")[-1].text,
             'summary': entry.find("p", first=True).text,
-            'image': entry.find("img.itemcov", first=True).text,
+            #'image': entry.find("a img.itemcov", first=True).attrs['src'],
+            # 無法取得 img attrs src，改從data-original attr取得
+            #'image': entry.find("a img"),
             'link': "https:" + entry.find("a[rel='mid_image']", first=True).attrs['href']
 
         }
+        # author list: 處理當同時有作者、譯者、插畫家等複數創作者存在時
+        author_list = entry.find("a[rel='go_author']")
+        if len(author_list) > 1:
+            for author in author_list[1:]:
+                meta['author'] += ' ' + author.text
+
+        # price list: 可能有不打折的情況
+        if len(entry.find("span.price strong b")) < 2:
+            meta['discount'] = "優惠價"
+
         # for data
         match_date = re.search("[0-9]{4}-[0-9]{2}-[0-9]{2}", entry.text)
         meta['date'] = match_date.group()
 
+        # for image
+        image_origin_link = entry.find("a img.itemcov", first=True).attrs['data-original']
+        match_img = re.search("https://www.books.com.tw/img/.+&w", image_origin_link)
+        if not match_img:
+            meta['image'] = "暫無圖片~"
+        # remove scaling
+        else:
+            meta['image'] = match_img.group()[:-2]
+
         return meta
+
+    def entry_filter(self, entry, option={"type": "chinese-only"}):
+        '''
+        剔除部分不吻合之選項
+        '''
+        booktype = entry.find("span.cat", first=True).text
+        if "type" in option and option['type'] == "chinese-only" and booktype != "中文書":
+            return False
+        
+        if "author" in option:
+            bookauthor = entry.find("a[rel='go_author']")
+            valid = False
+            if len(bookauthor) > 1:
+                for author_i in bookauthor:
+                    if option['author'] == author_i.text:
+                        valid = True
+                        break
+            else:
+                if option['author'] == bookauthor.text:
+                    valid = True
+            if not valid:
+                return False
+
+        if 'publish' in option and option['publish'] != entry.find("a[rel='mid_publish']", first=True).text:
+            return False
+        
+        return True
 
 # calculate Chinese sentenses' true length in python print function
 widths = [
@@ -193,9 +242,54 @@ def get_width(s):
 if __name__ == '__main__':
     
     '''
-    Practice mode: use fixed url as temporary use
+    Command mode: 現在支援文字輸入搜尋了!
     '''
-    start_url = "https://search.books.com.tw/search/query/key/東野圭吾/cat/all"
+    start_url = "https://search.books.com.tw/search/query/cat/bka/key/[input]/sort/[seq]/"
+    # 參考搜尋url： 找作者、依上市日期排列
+    # start_url_date_author = "https://search.books.com.tw/search/query/key/東野圭吾/sort/5/adv_author/1"
+
+    search_input = input("請輸入欲搜尋的關鍵字: ")
+    while not search_input:
+        search_input = input("請輸入欲搜尋的關鍵字: ")
+
+    start_url = start_url.replace("[input]", search_input)
+    print(start_url)
+
+    # 進階搜尋欄位
+    option = {"type": "chinese"}
+    adv_filter = input("篩選欄位: 1. 作者 2. 出版社 3. 書名包括此關鍵字 (不輸入or錯誤輸入視為預設)  : ")
+    if adv_filter == "1":
+        start_url += "adv_author/1"
+        option["author"] = search_input
+    elif adv_filter == "2":
+        start_url += "adv_pub/1"
+        option["publish"] = search_input
+    elif adv_filter == "3":
+        start_url += "adv_title/1"
+
+    # 排序
+    adv_seq = input("排序: 1. 預設關聯度 2. 出版時間 3. 暢銷度 (不輸入or錯誤輸入視為預設選項->1)  : ")
+    if adv_seq == "2":
+        adv_seq = "5"
+    elif adv_seq == "3":
+        adv_seq = "9"
+    else:
+        adv_seq = "1"
+    
+    start_url = start_url.replace("[seq]", adv_seq)
+    
+    print(start_url)
+    os.system("pause")
+
+    '''
+    預期功能: 
+    可選擇查詢-> "書名(系列)" "作者" "出版社" 
+    排序選項-> 出版時間(固定新的在前) 暢銷度(固定熱賣的在前)
+    顯示-> 第一頁，視使用者是否要繼續搜尋
+    模糊度-> 書名(使用博客來預設) 作者(作者欄位須有符合欲查找之目標方呈現)
+    項目-> 暫時只允許 1. 圖書類 2. 中文書項目(排除外文、簡體)
+
+    '''
 
     booksParser = BooksParser(start_url)
     numPages = 1
@@ -203,12 +297,20 @@ if __name__ == '__main__':
     metapage = booksParser.fetch(start_url)
     entries = booksParser.parse_article_entries(metapage.text)
 
+    if not entries:
+        print("\n找不到此關鍵字之資料!\n")
+
+    print("------------------------------")
     for entry in entries:
+        if not booksParser.entry_filter(entry, option=option):
+            continue
+        print()
         meta = booksParser.parse_article_meta(entry)
-        print("{}\n{} {} {}\n{} {}\n{}\nLink: {}".format(meta['title'], meta['author'], meta['publish'], meta['date'], meta['discount']+"折", meta['price']+'元', 
-            meta['summary'], meta['link']))
+        print("{}\n{} {} {}\n{} {}\n{}\nLink: {}\nImage link: {}".format(meta['title'], meta['author'], meta['publish'], meta['date'], meta['discount'], meta['price']+'元', 
+            meta['summary'], meta['link'], meta['image']))
         
-        break
+        print()
+        print("------------------------------")
 
     '''
     meta_data = pttParser.get_pages_meta(start_url, numPages)
